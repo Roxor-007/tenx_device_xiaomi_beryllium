98368247961a661c6080fd177a11538924d5b10b
Roxor-007 <koyande72@gmail.com>
2021-05-06 01:24:50 +0530

    aosp bringup

--- aosp_beryllium.mk
+++ aosp_beryllium.mk
@@ -7,11 +7,11 @@
 $(call inherit-product, device/xiaomi/beryllium/device.mk)
 
 # Inherit some common Dot OS stuff.
-$(call inherit-product, vendor/dot/config/common.mk)
+$(call inherit-product, vendor/aosp/config/common.mk)
 TARGET_INCLUDE_PIXEL_CHARGER := true
 
 # Device identifier. This must come after all inclusions.
-PRODUCT_NAME := dot_beryllium
+PRODUCT_NAME := aosp_beryllium
 PRODUCT_DEVICE := beryllium
 PRODUCT_BRAND := Xiaomi
 PRODUCT_MODEL := POCO F1
